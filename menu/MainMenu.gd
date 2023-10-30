2345de8b6b05515886480750068789e735a42a0c
GlitchyTriPod <tfrowan@insightbb.com>
2023-10-30 12:25:21 -0400

    added options screen and functional audio options (sliders)

--- menu/MainMenu.gd
+++ menu/MainMenu.gd
@@ -2,8 +2,14 @@ class_name MainMenu extends CanvasLayer
 
 signal new_game
 
+const OptionsMenu = preload("res://menu/OptionsMenu.tscn")
+
 @export var new_game_dialogue: DialogueResource
 @export var new_game_title: String = ""
+
+@onready var control: Control = %Control
+@onready var new_game_button: Button = %NewGame
+@onready var options_button: Button = %Options
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +17,7 @@ func _ready():
 
 	var tween = create_tween()
 	tween.tween_property(
-		%Control,
+		self.control,
 		"modulate",
 		Color("#ffffff"),
 		0.5
@@ -20,7 +26,7 @@ func _ready():
 func delete_self():
 	var tween = create_tween()
 	await tween.tween_property(
-		%Control,
+		self.control,
 		"modulate",
 		Color("#ffffff00"),
 		0.5
@@ -36,3 +42,44 @@ func _on_new_game_button_up():
 	get_node("/root/SceneManager").load_dialogue(self.new_game_title, self.new_game_dialogue)
 
 	delete_self()
+
+func _on_options_button_up():
+	get_node("/root/AudioManager").play_sfx("selectblip2")
+
+	# remove focus from all elements on the main menu
+	self.control.grab_focus()
+	self.control.release_focus()
+
+	# spawn options screen (this SHOULD block all interaction with the main menu)
+	var options_menu = OptionsMenu.instantiate()
+
+	# options_menu.get_node("Control/color").self_modulate = Color("#ffffff00")
+	options_menu.get_node("Control").modulate = Color("#ffffff00")
+
+	# OPTIoNALLY disable all buttons
+	# connect exit signal to function that restores interactivity with buttons
+	self.new_game_button.disabled = true
+	self.options_button.disabled = true
+
+	options_menu.connect("tree_exiting", func():
+		self.new_game_button.disabled = false
+		self.options_button.disabled = false
+
+		var tween = create_tween()
+		tween.tween_property( \
+		get_node("Control/overlay"), \
+		"modulate", \
+		Color("#ffffff00"), \
+		0.5 \
+		)
+	)
+
+	var tween = create_tween()
+	tween.tween_property( \
+		get_node("Control/overlay"), \
+		"modulate", \
+		Color("#ffffffff"), \
+		0.5 \
+	)
+
+	self.add_child(options_menu)
